912a899356431973c30f9a1688d4669e65f7fd4c
yeohaein <haaaein@naver.com>
2021-02-02 16:37:11 +0900

    Handling Routes with Express

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+/**
+ * 서버를 생성하고 route를 생성하고 그리고 그것에 응답하는 방식.
+ * express와 node.js로 할 수 있음.
+ */
+
 const express = require('express');
 const app = express();
 
@@ -6,5 +11,17 @@ const PORT = 4000;
 function handleListening() {
     console.log(`Listening on: http://localhost:${PORT}`);
 }
+
+function handleHome(req, res) {
+    console.log(req);
+    res.send("Hello from Home");
+}
+
+function handleProfile(req, res) {
+    res.send("You are on my profile");
+}
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
 
 app.listen(PORT, handleListening);
