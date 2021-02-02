ec8bad9d4239e89dc4618f828cbcddc4f7671f05
yeohaein <haaaein@naver.com>
2021-02-02 17:02:26 +0900

    ES6 on NodeJS using Babel

--- index.js
+++ index.js
@@ -3,23 +3,17 @@
  * express와 node.js로 할 수 있음.
  */
 
-const express = require('express');
+import express from "express";
 const app = express();
 
 const PORT = 4000; 
 
-function handleListening() {
-    console.log(`Listening on: http://localhost:${PORT}`);
-}
+const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
 
-function handleHome(req, res) {
-    console.log(req);
-    res.send("Hello from Home");
-}
+const handleHome = (req, res) => res.send("Hello from Haein");
 
-function handleProfile(req, res) {
-    res.send("You are on my profile");
-}
+const handleProfile = (req, res) => res.send("You are on my profile");
+
 app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
