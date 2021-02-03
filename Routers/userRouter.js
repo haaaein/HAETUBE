326589f987fb5482b7a03d86461d6a17f4521ce5
yeohaein <haaaein@naver.com>
2021-02-03 18:20:33 +0900

    MVC Pattern part One

---
+++ Routers/userRouter.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const userRouter = express.Router();
+
+export default userRouter;
+
+/**
+ * M data
+ * V how does the data look
+ * C function that looks for the data
+ */
--- app.js
+++ app.js
@@ -9,24 +9,21 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { userRouter } from "./router";
+import { userRouter } from "./userRouter";
+import userRouter from "./Routers/userRouter";
+import videoRouter from "./Routers/videoRouter";
+import globalRouter from "./Routers/globalRouter";
 const app = express();
 
-const handleHome = (req, res) => res.send("Hello from Haein");
-
-const handleProfile = (req, res) => res.send("You are on my profile");
-
+//use는 누군가가 접속하면 이 router 전체를 사용하겠다는 의미
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded( { extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.get("/", handleHome);
-
-app.get("/profile", handleProfile);
-
+app.use("/", globalRouter);
 app.use("/user", userRouter);
-//use는 누군가가 /user에 접속하면 이 router 전체를 사용하겠다는 의미
+app.use("/video", videoRouter);
 
 export default app;
