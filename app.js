ff6cee96ac5f32f2c26141303b9c720962aecd3b
yeohaein <haaaein@naver.com>
2021-02-08 21:29:11 +0900

    Home Controller part Two

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ const app = express();
 app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded( { extended: true }));
-app.use(helmet());
+app.use(bodyParser.urlencoded( { extended: true } ));
+app.use(helmet( { contentSecurityPolicy: false } ));
 app.use(morgan("dev"));
 app.use(localsMiddleware);
 
