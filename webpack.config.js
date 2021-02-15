81325697c82b19ecf980f1bc7048599a7f9eec65
yeohaein <haaaein@naver.com>
2021-02-15 11:28:55 +0900

    Styles with Webpack part One

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const { Model } = require("mongoose");
+const path = require("path");
+const ExtractCSS = require("extract-text-webpack-plugin");
+
+const mode = process.env.WEBPACK_ENV;
+const ENTRY_FILE = path.resolve(_dirname, "assets", "js", "main.js");
+const OUTPUT_DIR = path.join(_dirname, "static");
+
+const config = {
+    entry: ENTRY_FILE,
+    mode: MODE,
+    module: {
+        rules: [
+            {
+                test: /\.(scss)$/,
+                use: ExtractCSS.extract([
+                    {
+                        loader: "css-loader"
+                    },
+                    {
+                        loader: "postcss-loader"
+                    },
+                    {
+                        loader: "sass-loader"
+                    }
+                ])
+            }
+        ]
+    },
+    output: {
+        path: OUTPUT_DIR,
+        filename: "[name].[format]"
+    }
+};
+
+module.exports = config;
